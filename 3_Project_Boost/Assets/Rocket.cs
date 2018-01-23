15c5bda3dc66e07d991201d25a904842103e91d7
Sean <seanmbrage@me.com>
2018-01-22 20:51:26 -0700

    added thruster sound and rotation controls

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource thrusterSound;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        thrusterSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,15 +22,25 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if (!thrusterSound.isPlaying)
+            {
+                thrusterSound.Play();
+            }
             rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            thrusterSound.Stop();
         }
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            transform.Rotate(Vector3.forward);
             print("Rotating left");
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            transform.Rotate(Vector3.back);
             print("Rotating right");
         }
     }
